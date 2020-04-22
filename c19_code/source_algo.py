883a0fb6feb2398b110255f9adc4b01cb540f0b9
datajms <jean.matthieu.schertzer@gmail.com>
2020-04-22 09:45:46 +0200

    change gitignore, set-up pre-commit

--- c19_code/source_algo.py
+++ c19_code/source_algo.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from pdb import set_trace
 
-def compute_row_algo(df):
+
+def compute_row_algo(df: pd.Series) -> str:
     """This algorithm is based on recommandations of AP-HP and the Pasteur
     Institute, on Friday, the 30th of March 2020.
     Check https://delegation-numerique-en-sante.github.io/covid19-algorithme-orientation/algorithme-orientation-covid19.html
@@ -15,61 +16,63 @@ def compute_row_algo(df):
         A row of a dataframe. Columns should strictly match names in the conditions below
     """
 
-    output=""
+    output = ""
 
-    if df["facteurs_gravite_majeur__au_moins_1"]:#L10
-        output = "FIN5"#L11
+    if df["major_severity_factor__1_or_more"]:  # L10
+        output = "END5"  # L11
 
-    elif df["fievre__oui"] and df["toux__oui"]:#L14
-        if df["facteurs_pronostiques__0"]:#L15
-            output = "FIN6"#L16
-        elif df["facteurs_pronostiques__au_moins_1"]:#L18
-            if df["facteurs_gravite_mineur__0"] or \
-                df["facteurs_gravite_mineur__1"]:#L19
-                output = "FIN6"#L20
-            elif df["facteurs_gravite_mineur__2"]:#L22
-                output = "FIN4"#L23
+    elif df["fever__yes"] and df["couch__yes"]:  # L14
+        if df["risk_factor__0"]:  # L15
+            output = "END6"  # L16
+        elif df["risk_factor__1_or_more"]:  # L18
+            if df["minor_severity_factor__0"] or df["minor_severity_factor__1"]:  # L19
+                output = "END6"  # L20
+            elif df["minor_severity_factor__2"]:  # L22
+                output = "END4"  # L23
             else:
-                output = "ERROR"#This branch should not be possible
+                output = "ERROR"  # This branch should not be possible
         else:
-            output = "ERROR"#This branch should not be possible
+            output = "ERROR"  # This branch should not be possible
 
-    elif df["fievre__oui"] or df["diarrhee__oui"] or \
-        (df["toux__oui"] and df["douleurs__oui"]) or \
-        (df["toux__oui"] and df["anosmie__oui"]):#L28
-        if df["facteurs_pronostiques__0"]:#L29
-            if df["facteurs_gravite_mineur__0"]:#L30
-                if df["age__moins_de_50"]:#L31
-                    output = "FIN2"#L32
-                elif df["age__plus_de_50"]:#L34
-                    output = "FIN3"#L35
+    elif (
+        df["fever__yes"]
+        or df["diarrhea__yes"]
+        or (df["couch__yes"] and df["sore_throat_aches__yes"])
+        or (df["couch__yes"] and df["anosmia__yes"])
+    ):  # L28
+        if df["risk_factor__0"]:  # L29
+            if df["minor_severity_factor__0"]:  # L30
+                if df["age__49_or_less"]:  # L31
+                    output = "END2"  # L32
+                elif df["age__50_or_more"]:  # L34
+                    output = "END3"  # L35
                 else:
-                    output = "ERROR"#This branch should not be possible
-            elif df["facteurs_gravite_mineur__1"] or df["facteurs_gravite_mineur__2"]:#L38
-                output = "FIN3"#L39
+                    output = "ERROR"  # This branch should not be possible
+            elif (
+                df["minor_severity_factor__1"] or df["minor_severity_factor__2"]
+            ):  # L38
+                output = "END3"  # L39
             else:
-                output = "ERROR"#This branch should not be possible
-        elif df["facteurs_pronostiques__au_moins_1"]:#L42
-            if df["facteurs_gravite_mineur__0"] or df["facteurs_gravite_mineur__1"]:#L43
-                output = "FIN3"#L44
-            elif df["facteurs_gravite_mineur__2"]:#L46
-                output = "FIN4"#L47
+                output = "ERROR"  # This branch should not be possible
+        elif df["risk_factor__1_or_more"]:  # L42
+            if df["minor_severity_factor__0"] or df["minor_severity_factor__1"]:  # L43
+                output = "END3"  # L44
+            elif df["minor_severity_factor__2"]:  # L46
+                output = "END4"  # L47
             else:
-                output = "ERROR"#This branch should not be possible
+                output = "ERROR"  # This branch should not be possible
         else:
-            output = "ERROR"#This branch should not be possible
+            output = "ERROR"  # This branch should not be possible
 
-    elif df["toux__oui"] or df["douleurs__oui"] or df["anosmie__oui"]:#L52
-        if df["facteurs_pronostiques__0"]:#L53
-            output = "FIN2"#L54
-        elif df["facteurs_pronostiques__au_moins_1"]:#L56
-            output = "FIN7"#L57
+    elif df["couch__yes"] or df["sore_throat_aches__yes"] or df["anosmia__yes"]:  # L52
+        if df["risk_factor__0"]:  # L53
+            output = "END2"  # L54
+        elif df["risk_factor__1_or_more"]:  # L56
+            output = "END7"  # L57
         else:
-            output = "ERROR"#This branch should not be possible
-    else:#L61
-        output = "FIN8"#L62
-
-
+            output = "ERROR"  # This branch should not be possible
+    else:  # L61
+        output = "END8"  # L62
 
     if output == "ERROR":
         print("---ERROR: Impossible branch in source_algo---")
@@ -77,14 +80,9 @@ def compute_row_algo(df):
         set_trace()
 
     return output
-    
 
 
-def compute_df_algo(df):
-
-    output = df.apply(
-        lambda x: compute_row_algo(x),
-        axis=1
-    )
+def compute_df_algo(df: pd.DataFrame) -> pd.Series:
+    output = df.apply(lambda x: compute_row_algo(x), axis=1)
 
     return output
